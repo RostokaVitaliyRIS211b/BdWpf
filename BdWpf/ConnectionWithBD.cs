8524bd4e68a4e2184afdea567b89e425334e522d
Виталий Ростока <rostoka.vit@yandex.ru>
2025-01-02 20:32:11 +0500

    Добавьте файлы проекта.

---
+++ BdWpf/ConnectionWithBD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdWpf
+{
+    public static class ConnectionWithBD
+    {
+
+        static ConnectionWithBD()
+        {
+
+        }
+    }
+}
