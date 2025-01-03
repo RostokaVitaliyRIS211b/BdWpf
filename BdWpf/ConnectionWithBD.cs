1064de7ae49bb316ab32225686fa2283414e1456
Виталий Ростока <rostoka.vit@yandex.ru>
2025-01-03 19:15:48 +0500

    Добавил подключение к существующей базе данных через EF

--- BdWpf/ConnectionWithBD.cs
+++ BdWpf/ConnectionWithBD.cs
@@ -1,17 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BdWpf
+public class ApplicationContext : DbContext
 {
-    public static class ConnectionWithBD
+    public DbSet<SubscriptionType> subscriptions { get; set; } = null!;
+    public DbSet<SubscriberType> subscribers { get; set; } = null!;
+    public DbSet<JournalType> journals { get; set; } = null!;
+    public DbSet<LitType> literatureoragnizations { get; set; } = null!;
+    public DbSet<TypeOf> statusesofsubscription { get; set; } = null!;
+    public DbSet<TypeOf> statusesofdelivery { get; set; } = null!;
+    public DbSet<TypeOf> namesofjournals { get; set; } = null!;
+    public DbSet<TypeOf> typesofjournals { get; set; } = null!;
+    public DbSet<TypeOf> typesoforganizations { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
     {
-
-        static ConnectionWithBD()
-        {
-
-        }
+        Database.EnsureCreated();
+    }
+}
+public class TypeOf
+{
+    [Column("id")]
+    public int id { get; set; }
+    public string? Type { get; set; }
+    public override string ToString()
+    {
+        return $"{id} {Type}";
+    }
+}
+public class LitType
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("idoftype")]
+    public int IdOfType { get; set; }
+    [Column("name")]
+    public string? name { get; set; }
+    public override string ToString()
+    {
+        return $"{Id} {IdOfType} {name}";
+    }
+}
+public class JournalType
+{
+    [Column("id")]
+    public int id { get; set; }
+    public int countofsubs { get; set; }
+    public int idoforg { get; set; }
+    public int idofname { get; set; }
+    public int idoftype { get; set; }
+    public override string ToString()
+    {
+        return $"{id} {countofsubs} {idoforg} {idofname} {idoftype}";
+    }
+}
+public class SubscriberType
+{
+    [Column("id")]
+    public int id { get; set; }
+    public string surname { get; set; }
+    public string name { get; set; }
+    public string patronymic { get; set; }
+    public string street { get; set; }
+    public int numberofhouse { get; set; }
+    public int numberofapartment { get; set; }
+    public string litera { get; set; }
+    public override string ToString()
+    {
+        return $"{id} {surname} {name} {patronymic} {street} {numberofhouse} {numberofapartment} {litera}";
+    }
+}
+public class SubscriptionType
+{
+    [Column("id")]
+    public int id { get; set; }
+    public DateTime dateofend { get; set; }
+    public DateTime dateofdelivery { get; set; }
+    public int idofstatusdelivery { get; set; }
+    public int idofstatussubs { get; set; }
+    public int idofjournal { get; set; }
+    public int idofsubscriber { get; set; }
+    public override string ToString()
+    {
+        return $"{id} {dateofend} {dateofdelivery} {idofstatusdelivery} {idofstatussubs} {idofjournal} {idofsubscriber}";
     }
 }
--- BdWpf/MainWindow.xaml.cs
+++ BdWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +9,27 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace BdWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        ApplicationContext db = null;
         public MainWindow()
         {
             InitializeComponent();
+            
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=userdb;Username=postgres;Password=7331").Options;
+            db= new ApplicationContext(options);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Database.CloseConnection();
+            db.Dispose();
+            db=null;
         }
     }
 }
